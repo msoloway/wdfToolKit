04cfc260a384767675d9b4ba4de2cfede8957491
melissathompson24 <melissa.thompson24@gmail.com>
2016-10-14 19:58:50 -0600

    initial post missing action plan and full manual

---
+++ js/custom.js
@@ -0,0 +1,143 @@
+$(document).ready(function(){
+
+$('#click_advance0').click(function(){
+  $('#display_advance0').toggle('500');
+  $('#display_advance0').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance1').click(function(){
+  $('#display_advance1').toggle('500');
+  $('#display_advance1').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance1-1').click(function(){
+  $('#display_advance1-1').toggle('500');
+  $('#display_advance1-1').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance1-2').click(function(){
+  $('#display_advance1-2').toggle('500');
+  $('#display_advance1-2').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance1-3').click(function(){
+  $('#display_advance1-3').toggle('500');
+  $('#display_advance1-3').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance1-4').click(function(){
+  $('#display_advance1-4').toggle('500');
+  $('#display_advance1-4').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+
+
+
+
+$('#click_advance2').click(function(){
+  $('#display_advance2').toggle('500');
+  $('#display_advance2').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance2-1').click(function(){
+  $('#display_advance2-1').toggle('500');
+  $('#display_advance2-1').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance2-2').click(function(){
+  $('#display_advance2-2').toggle('500');
+  $('#display_advance2-2').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance2-3').click(function(){
+  $('#display_advance2-3').toggle('500');
+  $('#display_advance2-3').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance2-4').click(function(){
+  $('#display_advance2-4').toggle('500');
+  $('#display_advance2-4').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance2-5').click(function(){
+  $('#display_advance2-5').toggle('500');
+  $('#display_advance2-5').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance2-6').click(function(){
+  $('#display_advance2-6').toggle('500');
+  $('#display_advance2-6').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance2-7').click(function(){
+  $('#display_advance2-7').toggle('500');
+  $('#display_advance2-7').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance2-8').click(function(){
+  $('#display_advance2-8').toggle('500');
+  $('#display_advance2-8').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance2-9').click(function(){
+  $('#display_advance2-9').toggle('500');
+  $('#display_advance2-9').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+$('#click_advance2-10').click(function(){
+  $('#display_advance2-10').toggle('500');
+  $('#display_advance2-10').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+
+
+
+
+$('#click_advance3').click(function(){
+  $('#display_advance3').toggle('500');
+  $('#display_advance3').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+
+$('#click_advance4').click(function(){
+  $('#display_advance4').toggle('500');
+  $('#display_advance4').toggleClass("show-display");
+  $("i",this).toggleClass("fa-plus fa-minus");
+});
+
+
+// $('#collapse1').click(function(){
+//     $(this).next('div').slideToggle('500');
+//     $(this).find('i').toggleClass('fa-plus fa-minus');
+//     console.log("yes");
+// });
+
+
+
+// $('#collapseDiv2').on('shown.bs.collapse', function () {
+//        $(".fa").removeClass("fa-plus").addClass("fa-minus");
+//     });
+
+//     $('#collapseDiv2').on('hidden.bs.collapse', function () {
+//        $(".fa").removeClass("fa-minus").addClass("fa-plus");
+//     });
+
+}); // close out script
